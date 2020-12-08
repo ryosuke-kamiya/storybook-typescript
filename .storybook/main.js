6975702388a48f20c52c9406798b99ed84b8b7bc
神谷 <cinesuija@gmail.com>
2020-12-08 19:20:42 +0900

    ejectまえ

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,14 @@
+const path = require('path')
+
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/Index.tsx',
+  output: {
+    filename: 'bundle/main.[chunkhash].js',
+    path: path.resolve('public'),
+    publicPath: '/public'
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -7,5 +17,22 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal(config){//storybookはデフォルトでTSに対応していないので、これを追記するひつようがある。
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+          options: {
+            transpileOnly: true
+          }
+        }
+      ]
+    })
+
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    return config;
+  }
 }
